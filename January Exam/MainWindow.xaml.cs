4facde2a05a77d8e911608115b20f2442e39cf54
CelticWizard <35495540+CelticWizard@users.noreply.github.com>
2022-01-07 18:04:26 +0000

    Q1(j)

--- January Exam/MainWindow.xaml.cs
+++ January Exam/MainWindow.xaml.cs
@@ -27,6 +27,9 @@ namespace January_Exam
             InitializeComponent();
         }
         List<Member> members = new List<Member>();
+        List<JuniorMember> jMembers = new List<JuniorMember>();
+        List<SeniorMember> sMembers = new List<SeniorMember>();
+        List<Member> regMembers = new List<Member>();
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             //Member details from moodle
@@ -34,14 +37,24 @@ namespace January_Exam
             JuniorMember jm1 = new JuniorMember() { Name = "Jack Murphy", Fee = 1000, JoinDate = new DateTime(2020, 5, 7), PaymentType = PaymentSchedule.Annual };
             JuniorMember jm2 = new JuniorMember() { Name = "Emily Kelly", Fee = 1000, JoinDate = new DateTime(2021, 1, 10), PaymentType = PaymentSchedule.Biannual };
             JuniorMember jm3 = new JuniorMember() { Name = "Daniel Ryan", Fee = 1000, JoinDate = new DateTime(2020, 11, 30), PaymentType = PaymentSchedule.Monthly };
+            jMembers.Add(jm1);
+            jMembers.Add(jm2);
+            jMembers.Add(jm3);
 
             Member m1 = new Member() { Name = "Ella Doyle", Fee = 1000, JoinDate = new DateTime(2019, 3, 20), PaymentType = PaymentSchedule.Annual };
             Member m2 = new Member() { Name = "Fionn Kennedy", Fee = 1000, JoinDate = new DateTime(2018, 8, 15), PaymentType = PaymentSchedule.Biannual };
             Member m3 = new Member() { Name = "Louise Moore", Fee = 1000, JoinDate = new DateTime(2017, 2, 10), PaymentType = PaymentSchedule.Monthly };
+            regMembers.Add(m1);
+            regMembers.Add(m2);
+            regMembers.Add(m3);
 
             SeniorMember sm1 = new SeniorMember() { Name = "Cian Daly", Fee = 1000, JoinDate = new DateTime(2015, 4, 24), PaymentType = PaymentSchedule.Annual };
             SeniorMember sm2 = new SeniorMember() { Name = "Bobby Quinn", Fee = 1000, JoinDate = new DateTime(2014, 12, 1), PaymentType = PaymentSchedule.Biannual };
             SeniorMember sm3 = new SeniorMember() { Name = "Eve Gallagher", Fee = 1000, JoinDate = new DateTime(2013, 6, 1), PaymentType = PaymentSchedule.Monthly };
+            sMembers.Add(sm1);
+            sMembers.Add(sm2);
+            sMembers.Add(sm3);
+
             //Adding all details to the members list
             members.Add(jm1);
             members.Add(jm2);
@@ -59,7 +72,34 @@ namespace January_Exam
         private void lbxMemberList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Member selected = lbxMemberList.SelectedItem as Member;//The selected members' details can be accessed now
-            txtMemberDetails.Text = $"{selected.Name}\nJoin date: {selected.JoinDate}\nBasic fee: {selected.Fee}\nPayment schedule: {selected.PaymentType} €{selected.CalculateFees().ToString("0.00")}\nRenewal date: {selected.RenewalDate().ToShortDateString()}\nDays to renewal: {selected.DaysToRenewal()}\n";
+            if (selected != null)
+            {
+                txtMemberDetails.Text = $"{selected.Name}\nJoin date: {selected.JoinDate}\nBasic fee: {selected.Fee}\nPayment schedule: {selected.PaymentType} €{selected.CalculateFees().ToString("0.00")}\nRenewal date: {selected.RenewalDate().ToShortDateString()}\nDays to renewal: {selected.DaysToRenewal()}\n";
+            }
+        }
+
+        private void radioAll_Checked(object sender, RoutedEventArgs e)
+        {
+            lbxMemberList.ItemsSource = null;
+            lbxMemberList.ItemsSource = members;
+        }
+
+        private void radioRegular_Checked(object sender, RoutedEventArgs e)
+        {
+            lbxMemberList.ItemsSource = null;
+            lbxMemberList.ItemsSource = regMembers;
+        }
+
+        private void radioSenior_Checked(object sender, RoutedEventArgs e)
+        {
+            lbxMemberList.ItemsSource = null;
+            lbxMemberList.ItemsSource = sMembers;
+        }
+
+        private void radioJunior_Checked(object sender, RoutedEventArgs e)
+        {
+            lbxMemberList.ItemsSource = null;
+            lbxMemberList.ItemsSource = jMembers;
         }
     }
 }
